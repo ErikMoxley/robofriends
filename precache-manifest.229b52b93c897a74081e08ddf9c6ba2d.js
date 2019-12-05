0c79709ee54fb8938826fcb90cfe28a7ebc404e8
ErickMoxley <exilerzero@yahoo.com>
2019-12-04 20:35:58 -0500

    Updates

---
+++ precache-manifest.229b52b93c897a74081e08ddf9c6ba2d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38ea98a8e5e854fc0a48ca87695c31a8",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "25165a93389fb0113ca7",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "073a90ebbc7a3e011b13",
+    "url": "/robofriends/static/css/main.f98a7f5f.chunk.css"
+  },
+  {
+    "revision": "25165a93389fb0113ca7",
+    "url": "/robofriends/static/js/2.7136d570.chunk.js"
+  },
+  {
+    "revision": "073a90ebbc7a3e011b13",
+    "url": "/robofriends/static/js/main.fcb2f191.chunk.js"
+  },
+  {
+    "revision": "5645dfe01f57d087ff9e",
+    "url": "/robofriends/static/js/runtime-main.47a7d18e.js"
+  },
+  {
+    "revision": "e924db9eca88feeaf3c55fc064f7b654",
+    "url": "/robofriends/static/media/Sketch_Block.e924db9e.woff"
+  }
+]);
